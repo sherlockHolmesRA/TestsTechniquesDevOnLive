2a9a2e2620ee572da2ebd3841374672b24f47eed
rihab attig <rihab.attig@gmail.com>
2022-04-27 17:22:05 +0200

    second commit test technique on live

--- src/main/java/com/teststechniques/devOnlive/Factorial.java
+++ src/main/java/com/teststechniques/devOnlive/Factorial.java
@@ -1,5 +1,7 @@
 package com.teststechniques.devOnlive;
 
+import java.util.stream.IntStream;
+
 public class Factorial {
 
 	public static int factorial(int n) {
@@ -10,8 +12,14 @@ public class Factorial {
 		return res;
 	}
 	
+	public static int factorial2(int n) {
+		int res = IntStream.rangeClosed(2, n).reduce(1, (x, y) -> x * y);
+		return res;
+	}
+	
 	public static void ResultFactorial(int f) {
 		System.out.println(factorial(f));
+		System.out.println(factorial2(f));
 	}
 	
 }
